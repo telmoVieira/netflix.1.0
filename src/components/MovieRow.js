ba6445f93167796a46bcc10c6994bc3ef211c0b3
Telmo Vieira <telmo_vieira2@hotmail.com>
2021-05-10 18:06:25 +0100

    Scroll Arrows

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React , { useEffect, useState }from 'react';
 import './MovieRow.css';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { withWidth } from '@material-ui/core';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({title, items}) => {
+export default ({ title, items }) => {
+    const[scrollX, setScrollX] = useState(-400);
+   
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x = 0){
+            x = 0;
+        }
+         setScrollX(x);
+    }
+
+    const handleRighttArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW =  items.results.length * 200;
+        if((window.innerWidth - listW) > x ) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
-            <div className = "movieRow--left">
-                <NavigateBeforeIcon style={{fontSize: 50}} />
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{ fontSize: 50 }} />
             </div>
-            <div className = "movieRow--right">
-                <NavigateNextIcon style={{fontSize: 50}} />
+            <div className="movieRow--right" onClick={handleRighttArrow}>
+                <NavigateNextIcon style={{ fontSize: 50 }} />
             </div>
             <div className="movieRow--listarea">
-                <div className = "movieRow--list" >
-                    {items.results.length > 0 && items.results.map((item, key)=>(
-                        <div key={key} className = "movieRow--item">
-                        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                <div className="movieRow--list" style ={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 200
+                }}>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                         </div>
                     ))}
                 </div>
